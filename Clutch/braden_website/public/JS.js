f50c178b576cf8aa41641399134b4ced3eada58d
Braden Lee <braden.lee96@gmail.com>
2020-02-28 11:27:57 -0800

    first test commit

---
+++ Clutch/braden_website/public/JS.js
@@ -0,0 +1,50 @@
+/*
+JS
+
+
+I added js function to simulate animation for navigation bar when scrolled to the very top
+-- Braden lee
+
+*/
+
+$(document).ready(function(){
+  header = $('.header');
+  head = $('.head');
+  navIcons = $('.navBar');
+
+    $(document).scroll(function(){
+      var top = $(this).scrollTop();
+      /*
+      if(top>4)
+      {
+        header.addClass('headerActive');
+        head.addClass('headActive');
+        navIcons.addClass('iconActive');
+      }
+      else
+      {
+        header.removeClass('headerActive');
+        header.addClass('header');
+        head.removeClass('headActive');
+        head.addClass('head');
+        navIcons.removeClass('iconActive');
+        navIcons.addClass('navBar');
+      }
+      */
+      if(top>4)
+      {
+        header.removeClass('headerActive');
+        header.addClass('header');
+        head.removeClass('headActive');
+        head.addClass('head');
+        navIcons.removeClass('iconActive');
+        navIcons.addClass('navBar');
+      }
+      else
+      {
+        header.addClass('headerActive');
+        head.addClass('headActive');
+        navIcons.addClass('iconActive');
+      }
+    })
+});
